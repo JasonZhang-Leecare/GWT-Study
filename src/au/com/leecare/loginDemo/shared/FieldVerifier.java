93b9a585dfd834387118b210d27ecea7a4520bab
Jason Zhang <jason.z@leecare.com.au>
2020-11-04 13:53:30 +1300

    Improve Field Verifier to improve performance

--- src/au/com/leecare/loginDemo/shared/FieldVerifier.java
+++ src/au/com/leecare/loginDemo/shared/FieldVerifier.java
@@ -6,57 +6,71 @@ package au.com.leecare.loginDemo.shared;
 // import java.util.regex.Pattern;
 
 /**
- * Validator Utility including validate: UserName - Not null, no special character. Password - at
- * least 6 digit; no special character; at least one upper case character; at least one digit.
+ * Validator Utility
+ *
+ * @auther Jason Zhang
  */
 public class FieldVerifier {
 
-  private static int ASCII_0 = 48;
-  private static int ASCII_9 = 57;
-  private static int ASCII_A = 65;
-  private static int ASCII_Z = 90;
-  private static int ASCII_a = 97;
-  private static int ASCII_z = 122;
+  private static final int ASCII_0 = 48;
+  private static final int ASCII_9 = 57;
+  private static final int ASCII_A = 65;
+  private static final int ASCII_Z = 90;
+  private static final int ASCII_a = 97;
+  private static final int ASCII_z = 122;
+  private static final int MIN_LENGTH_USER_NAME = 6;
 
-  /** Return true if username is not null and not cantains special character; false otherwise */
-  public static boolean isValidName(String name) {
-    if (name == null || name.length() < 6) {
+  /**
+   * Validate a User Name
+   *
+   * @param aUserName UserName to validate
+   * @return  true if username is not null and not cantains special character; false otherwise
+   * */
+  public static boolean isValidName(String aUserName) {
+    if (aUserName == null || aUserName.length() < MIN_LENGTH_USER_NAME) {
       return false;
     }
 
-    return noSpecialCharacter(name);
+    return notContainSpecialCharacter(aUserName);
   }
 
   // Will use Apache String Util to replace the function here
   /**
-   * Return true is password: at least 6 digit long; no special character; contains at least one
-   * upper case character; contains at least one digit.
+   * Validate Passowrd
+   *
+   * @param aPassword password to validate
+   * @return true if password:
+   *  is not blank;
+   *  contains at least one upper case character;
+   *  contains at least special character.
    */
-  public static boolean isValidPassword(String password) {
-    if (password == null || password.trim().isEmpty()) return false;
-    /*
+  public static boolean isValidPassword(String aPassword) {
 
-    Pattern regex = Pattern.compile("[^A-Za-z0-9]");
+    if (aPassword == null || aPassword.trim().isEmpty()) return false;
 
-    Matcher matcher = regex.matcher(password);
+    boolean containsSpecialCharacter = false;
+    boolean containsUpperCaseCharacter = false;
+    
+    for (char current : aPassword.toCharArray()) {
 
-    matcher.find();*/
+      if (!isNormalCharacter(current)) {
+        containsSpecialCharacter = true;
+      }
 
-    if (noSpecialCharacter(password)) return false;
+      if(isUpperCase(current)) {
+        containsUpperCaseCharacter = true;
+      }
 
-    if (!containsUpperCase(password)) return false;
+      if (containsSpecialCharacter && containsUpperCaseCharacter) break;
+    }
 
-    if (!containsDigit(password)) return false;
-
-    return true;
+    return containsSpecialCharacter && containsUpperCaseCharacter;
   }
 
   // Usually should use Apache StringUtil
-  private static boolean noSpecialCharacter(String input) {
-    for (int current : input.toCharArray()) {
-      if ((current >= ASCII_0 && current <= ASCII_9)
-          || (current >= ASCII_A && current <= ASCII_Z)
-          || (current >= ASCII_a && current <= ASCII_z)) {
+  private static boolean notContainSpecialCharacter(String aInput) {
+    for (char current : aInput.toCharArray()) {
+      if (isNormalCharacter(current)) {
         continue;
       } else {
         return false;
@@ -66,24 +80,13 @@ public class FieldVerifier {
     return true;
   }
 
-  // Usually should use Apache StringUtil
-  private static boolean containsUpperCase(String input) {
-    for (int current : input.toCharArray()) {
-      if (current >= ASCII_A && current <= ASCII_Z) {
-        return true;
-      }
-    }
-
-    return false;
+  private static boolean isNormalCharacter(char aChar) {
+    return (aChar >= ASCII_0 && aChar <= ASCII_9)
+            || (aChar >= ASCII_A && aChar <= ASCII_Z)
+            || (aChar >= ASCII_a && aChar <= ASCII_z);
   }
 
-  // Usually should use Apache StringUtil
-  private static boolean containsDigit(String input) {
-    for (int current : input.toCharArray()) {
-      if (current >= ASCII_0 && current <= ASCII_9) {
-        return true;
-      }
-    }
-    return false;
+  private static boolean isUpperCase(char aChar) {
+    return aChar >= ASCII_A && aChar <= ASCII_Z;
   }
 }
